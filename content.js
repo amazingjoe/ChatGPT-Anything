5346af5e5867c429509ef30f31b790f8dc736c47
amazingjoe <amazingjoe@users.noreply.github.com>
2023-01-28 18:13:13 -0800

    Fix code to allow for Save/Load of prompts

--- content.js
+++ content.js
@@ -1,5 +1,8 @@
+
+
 // btn_clipboardcopy is the ID of the button. When it is pressed we will copy the input value to the clipboard.
 document.getElementById("btn_clipboardcopy").addEventListener("click", function(){
+    console.log("Running clipboard!");
     var prompt = document.getElementById("prompt");
     var job = prompt.value;
     //var preprompt = "Add '[[[" + generateGUID() + "]]]\n' before the response and '\n[[[End]]]' at the end of the response.\n\n"; 
@@ -15,32 +18,18 @@ document.getElementById("btn_clipboardcopy").addEventListener("click", function(
 
 // btn_storeinfo is the ID of the button. When it is pressed we will store the input value using chrom.storage.local.
 document.getElementById("btn_storeinfo").addEventListener("click", function(){
-    var prompt = document.getElementById("prompt");
-    var entry = 'TEST!'; //prompt.value;
-    chrome.storage.local.set({'datastore': entry}, function() {
-        console.log("Value stored");
+    var prompt = document.getElementById("prompt").value;
+    console.log("Entry value is : " + prompt);
+    chrome.storage.local.set({'datastore': prompt}, function() {
+        console.log("Value stored is : " + prompt);
       });
 });
 
 // btn_storeinfo is the ID of the button. When it is pressed we will store the input value using chrom.storage.local.
 document.getElementById("btn_loadinfo").addEventListener("click", function(){
-    chrome.storage.local.get(['datastore'], function(result) {
-        if(chrome.runtime.lastError){
-            console.log("No value found")
-        }else if(result.myvalue){
-            // Value found lets display it in the input box
-            var prompt = document.getElementById("prompt");
-            prompt.value = result;
-
-        }else{
-            console.log("No value found")
-        }
-    });
+    chrome.storage.local.get(["datastore"]).then((result) => {
+        var prompt = document.getElementById("prompt");
+        prompt.value = result.datastore;
+        console.log("Value currently is " + result.datastore);
+      });
 });
-
-//Generate a static GUID for now
-function generateGUID() {
-    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
-      (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-    )
-  }
